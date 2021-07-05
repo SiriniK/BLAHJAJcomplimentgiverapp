dcb82a1fa3d734f4d8c5f7c6e2974fc7e6366a9a
Sirini Karunadasa <85049699+SiriniK@users.noreply.github.com>
2021-07-05 01:34:06 -0500

    Create BLAHJAJApp.swift

---
+++ BLAHJAJApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLAHJAJApp.swift
+//  BLAHJAJ
+//
+//  Created by Sirini Karunadasa on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BLAHJAJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
